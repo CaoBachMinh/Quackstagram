5dc12c1c1af1520e73a720521f21484565b0b05f
Ngo, Thien Tu (Stud. DACS) <tt.ngo@student.maastrichtuniversity.nl>
2025-02-17 18:39:50 +0000

    update codebase

---
+++ NavigationPanel.java
@@ -0,0 +1,104 @@
+import java.awt.Color;
+import java.awt.Image;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class NavigationPanel extends UIManager {
+    private UIManager parentFrame; 
+    public NavigationPanel(UIManager parentFrame) {
+        this.parentFrame = parentFrame;
+    }
+    JPanel createNavigationPanel() {
+        JPanel navigationPanel = new JPanel();
+        navigationPanel.setBackground(new Color(249, 249, 249));
+        navigationPanel.setLayout(new BoxLayout(navigationPanel, BoxLayout.X_AXIS));
+        navigationPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        navigationPanel.add(createIconButton("img/icons/home.png", "home"));
+        navigationPanel.add(Box.createHorizontalGlue());
+        navigationPanel.add(createIconButton("img/icons/search.png","explore"));
+        navigationPanel.add(Box.createHorizontalGlue());
+        navigationPanel.add(createIconButton("img/icons/add.png","add"));
+        navigationPanel.add(Box.createHorizontalGlue());
+        navigationPanel.add(createIconButton("img/icons/heart.png","notification"));
+        navigationPanel.add(Box.createHorizontalGlue());
+        navigationPanel.add(createIconButton("img/icons/profile.png", "profile"));
+
+        return navigationPanel;
+    }
+    private JButton createIconButton(String iconPath, String buttonType) {
+        ImageIcon iconOriginal = new ImageIcon(iconPath);
+        Image iconScaled = iconOriginal.getImage().getScaledInstance(NAV_ICON_SIZE, NAV_ICON_SIZE, Image.SCALE_SMOOTH);
+        JButton button = new JButton(new ImageIcon(iconScaled));
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setContentAreaFilled(false);
+    
+        // Define actions based on button type
+        if ("home".equals(buttonType)) {
+            button.addActionListener(e -> openHomeUI());
+        } else if ("profile".equals(buttonType)) {
+            button.addActionListener(e -> openProfileUI());
+        } else if ("notification".equals(buttonType)) {
+            button.addActionListener(e -> notificationsUI());
+        } else if ("explore".equals(buttonType)) {
+            button.addActionListener(e -> exploreUI());
+        } else if ("add".equals(buttonType)) {
+            button.addActionListener(e -> ImageUploadUI());
+        }
+        return button;
+    }
+    private void ImageUploadUI() {
+        // Open InstagramProfileUI frame
+        parentFrame.dispose();
+        ImageUploadUI upload = new ImageUploadUI();
+        upload.setVisible(true);
+    }
+
+
+    private void openProfileUI() {
+        // Open InstagramProfileUI frame
+        parentFrame.dispose();
+        String loggedInUsername = "";
+ 
+         // Read the logged-in user's username from users.txt
+    //  try (BufferedReader reader = Files.newBufferedReader(Paths.get("data", "users.txt"))) {
+    //      String line = reader.readLine();
+    //      if (line != null) {
+    //          loggedInUsername = line.split(":")[0].trim();
+    //      }
+    //  } catch (IOException e) {
+    //      e.printStackTrace();
+    //  }
+
+     LoggedinUser loggedinUser = LoggedinUser.getInstance();
+     loggedInUsername = loggedinUser.getUsername();
+
+      User user = new User(loggedInUsername);
+        InstagramProfileUI profileUI = new InstagramProfileUI(user);
+        profileUI.setVisible(true);
+    }
+ 
+     private void notificationsUI() {
+        // Open InstagramProfileUI frame
+        parentFrame.dispose();
+        NotificationsUI notificationsUI = new NotificationsUI();
+        notificationsUI.setVisible(true);
+    }
+ 
+    private void openHomeUI() {
+        // Open InstagramProfileUI frame
+        parentFrame.dispose();
+        QuakstagramHomeUI homeUI = new QuakstagramHomeUI();
+        homeUI.setVisible(true);
+    }
+ 
+    private void exploreUI() {
+        // Open InstagramProfileUI frame
+        parentFrame.dispose();
+        ExploreUI explore = new ExploreUI();
+        explore.setVisible(true);
+    }   
+}
